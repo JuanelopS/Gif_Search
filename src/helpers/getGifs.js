6acb55bac784de37b421b2db54864d93c3c4c01d
Juan Gavira <juan.gavira.pichaco@gmail.com>
2022-10-26 09:17:28 +0200

    api key hidden

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 // Si se usa el await este función tiene que ser async
 export const getGifs = async( category, page ) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=HTfrEZwhPGj4mEbIENI83ZSvOe5hBHqK&q=${ category }&limit=25&offset=${ page }`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=`/*{YOUR API HERE}*/`&q=${ category }&limit=25&offset=${ page }`;
     const resp = await fetch( url );
     const { data } = await resp.json();
 
